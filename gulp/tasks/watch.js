11b70d56b25171ff3567069c556bb83de5034ab7
Yegorich75 <yegorich75@gmail.com>
2017-12-07 18:34:51 +0200

    First commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,13 @@
+var gulp   = require('gulp');
+var config = require('../config');
+
+gulp.task('watch', 
+    ['copy:watch',
+    
+    'jade:watch',
+    'sprite:svg:watch',
+    'svgo:watch',
+    'list-pages:watch',
+    'webpack:watch',
+    'sass:watch'
+]);
